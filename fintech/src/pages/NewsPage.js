d1db19e9078a413acbcb692f7878b482978f2da8
kjungw1025 <kjungw1025@naver.com>
2023-08-01 21:24:52 +0900

    feat: Practice api using news api
    Adding event binding, state

--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderTitle from "../components/News/HeaderTitle";
 import SearchInput from "../components/News/SearchInput";
 import NewsList from "../components/News/NewsList";
 
 const NewsPage = () => {
-  return (
-    <div>
-        <HeaderTitle title={"뉴스검색"}></HeaderTitle>
-        <SearchInput></SearchInput>
-        <NewsList></NewsList>
-    </div>
-  )
-}
+    const [searchInput, setSearchInput] = useState("검색어");
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        setSearchInput(value);
+    };
+
+    const handleClick = () => {
+        console.log(searchInput);
+    };
+
+    return (
+        <div>
+            <HeaderTitle title={"뉴스검색"}></HeaderTitle>
+            <SearchInput handleChange={handleChange} handleClick={handleClick}></SearchInput>
+            <NewsList></NewsList>
+        </div>
+    );
+};
 
 export default NewsPage;
