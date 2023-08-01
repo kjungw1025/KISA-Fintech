3e1446c84b38f99a9f3dfc68dd50b840ea8c04c6
kjungw1025 <kjungw1025@naver.com>
2023-08-01 14:13:47 +0900

    feat: Practice props delivery

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Welcome = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>안녕하세요 {props.userName}님! {props.age}세 입니다.</p>
+    </div>
+  );
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome userName="홍길동" age={14}></Welcome>
     </div>
   );
 }
