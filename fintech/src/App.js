d94476ddd7c75e0558f9d8388aedef715fbba688
kjungw1025 <kjungw1025@naver.com>
2023-08-01 16:35:14 +0900

    feat: Practice creating a http request through Axios

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StyledComponent from "./components/StyledComponent";
 import ListComponent from "./components/ListComponent";
-import StateComponent from "./components/StateComponent";
+import Welcome from "./components/Welcome";
+import AxiosComponent from "./components/AxiosComponent";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/style" element={<StyledComponent/>}></Route>
+        <Route path="/" element={<Welcome/>}></Route>
+        <Route path="/axios" element={<AxiosComponent/>}></Route>
         <Route path="/list" element={<ListComponent/>}></Route>
-        <Route path="/state" element={<StateComponent/>}></Route>
+        <Route path="/style" element={<StyledComponent/>}></Route>
       </Routes>
     </BrowserRouter>
   );
