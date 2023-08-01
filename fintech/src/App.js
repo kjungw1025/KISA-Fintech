154bb738eb2dccd34b3985917b4d7a3b74a4f1d9
kjungw1025 <kjungw1025@naver.com>
2023-08-01 15:26:06 +0900

    feat: Practice listing

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import InputComponents from "./components/InputComponents";
+import ListComponent from './components/ListComponent';
 
 function App() {
   return (
     <div className="App">
       <InputComponents></InputComponents>
+      <ListComponent></ListComponent>
     </div>
   );
 }
---
+++ fintech/src/components/ListComponent.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Welcome from "./Welcome";
+
+const ListComponent = () => {
+    const [users, setUsers] = useState([
+        {username: "홍길동", age : 12, major : "소프트웨어"},
+        {username: "고길동", age : 22, major : "컴퓨터공학"},
+        {username: "김길동", age : 32, major : "모바일시스템공학"},
+    ]);
+
+    return (
+        <div>
+            {users.map((user) => {
+                return <Welcome userName={ user.username } age={ user.age }></Welcome>
+            })}
+        </div>
+    )
+}
+
+export default ListComponent
+// 자동 완성 : rafce, useStateSnippet
