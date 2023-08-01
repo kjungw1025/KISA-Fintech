4c9b14be64a11e579f656b754dd1f2dc998ac502
kjungw1025 <kjungw1025@naver.com>
2023-08-01 20:52:04 +0900

    feat: Practice api using news api
    Configure the default news search page

---
+++ fintech/src/components/News/NewsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewsList = ({ newList }) => {
+  return (
+    <div>{ }</div>
+  )
+}
+
+export default NewsList;
---
+++ fintech/src/components/News/NewsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeaderTitle from "../components/News/HeaderTitle";
+import SearchInput from "../components/News/SearchInput";
+import NewsList from "../components/News/NewsList";
+
+const NewsPage = () => {
+  return (
+    <div>
+        <HeaderTitle title={"뉴스검색"}></HeaderTitle>
+        <SearchInput></SearchInput>
+        <NewsList></NewsList>
+    </div>
+  )
+}
+
+export default NewsPage;
