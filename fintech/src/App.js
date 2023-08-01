025df2dce652e209ad5adc468af9cb26f47b724d
kjungw1025 <kjungw1025@naver.com>
2023-08-01 14:20:44 +0900

    feat: Practice separating components into separate files

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,14 +1,5 @@
 import './App.css';
-
-const Welcome = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>안녕하세요 {props.userName}님! {props.age}세 입니다.</p>
-    </div>
-  );
-};
-
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
