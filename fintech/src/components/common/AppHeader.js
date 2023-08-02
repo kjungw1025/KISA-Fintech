47f741985d0334c1acf1f1953f07a50f0f385af1
kjungw1025 <kjungw1025@naver.com>
2023-08-02 15:25:53 +0900

    feat: Create a Page Header

---
+++ fintech/src/components/common/AppHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #f2f2f2;
+  padding: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const AppHeader = ({ title }) => {
+  return <HeaderContainer>{title}</HeaderContainer>;
+};
+
+export default AppHeader;
