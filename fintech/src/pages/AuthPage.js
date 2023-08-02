b8f283caab50e426f24647c0ccc7b8fd9eae718a
kjungw1025 <kjungw1025@naver.com>
2023-08-02 15:21:12 +0900

    feat: Creating an authentication link page

---
+++ fintech/src/pages/AuthPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import AppHeader from '../components/common/AppHeader';
+
+const AuthPage = () => {
+    const handleClick = () => {
+        let tmpwindow = window.open("about:blank");
+        const clientId = process.env.REACT_APP_CLIENT_ID; //<-- 본인의 client id
+        tmpwindow.location.href = `https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0`;
+    };
+
+    return (
+        <div>
+        <AppHeader title={"인증시작"}> </AppHeader>
+        <button onClick={handleClick}>인증하기</button>
+        </div>
+    )
+}
+
+export default AuthPage
