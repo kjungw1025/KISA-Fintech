8a4356838b6d6c88315933c899f32a7502874994
kjungw1025 <kjungw1025@naver.com>
2023-08-02 15:48:57 +0900

    feat: Create Account List Lookup API

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StyledComponent from "./components/StyledComponent";
-import ListComponent from "./components/ListComponent";
-import Welcome from "./components/Welcome";
-import AxiosComponent from "./components/AxiosComponent";
-import NewsPage from "./pages/NewsPage";
+import AuthPage from "./pages/AuthPage";
+import AuthResultPage from "./pages/AuthResultPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome/>}></Route>
-        <Route path="/axios" element={<AxiosComponent/>}></Route>
-        <Route path="/list" element={<ListComponent/>}></Route>
-        <Route path="/style" element={<StyledComponent/>}></Route>
-        <Route path="/news" element={<NewsPage/>}></Route>
+        <Route path="/" element={<AuthPage/>}></Route>
+        <Route path="/main" element={<MainPage />}></Route>
+        <Route path="/authResult" element={<AuthResultPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
