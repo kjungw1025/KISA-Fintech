87fd2ad55deba1ed0833409530f1269b24106054
kjungw1025 <kjungw1025@naver.com>
2023-08-03 23:27:10 +0900

    feat: Update App.js

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -4,15 +4,19 @@ import AuthPage from "./pages/AuthPage";
 import AuthResultPage from "./pages/AuthResultPage";
 import MainPage from "./pages/MainPage";
 import BalancePage from "./pages/BalancePage";
+import QrCodePage from "./pages/QrCodePage";
+import QrReaderPage from "./pages/QrReaderPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthPage/>}></Route>
+        <Route path="/authResult" element={<AuthResultPage/>}></Route>
         <Route path="/main" element={<MainPage />}></Route>
         <Route path="/balance" element={<BalancePage />}></Route>
-        <Route path="/authResult" element={<AuthResultPage/>}></Route>
+        <Route path="/qrcode" element={<QrCodePage/>}></Route>
+        <Route path="/qrreader" element={<QrReaderPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
