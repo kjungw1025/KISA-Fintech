112be7d2efccb851dcb39675e120fcc540c4d806
kjungw1025 <kjungw1025@naver.com>
2023-08-03 23:26:43 +0900

    feat: Modify design qr reader page

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -5,8 +5,26 @@ import ModalWithdraw from "../components/Withdraw/ModalWithdraw";
 import Modal from "react-modal";
 
 const QrReaderPage = () => {
-  const [val, setVal] = useState("");
+  const [val, setVal] = useState(""); // 사용할 출금 이후 입금할 fintechUseNo
   const [openModal, setOpenModal] = useState(false); //no camera : true
+
+  const CustomStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+      zIndex: "9",
+    },
+    content: {
+      width: "95%",
+      border: `0 solid black`,
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      zIndex: "99999",
+    },
+  };
 
   const closeModal = () => {
       setOpenModal(false);
@@ -20,10 +38,15 @@ const QrReaderPage = () => {
     <div>
         <AppHeader title={"qr 리더"}></AppHeader>
         <QrCodeReader delay={100} width={600} height={500} action={handleRead} />
-        <Modal isOpen={openModal} onRequestClose={closeModal}>
-          <ModalWithdraw tofintechno={val}></ModalWithdraw>
+        <Modal
+          isOpen={openModal}
+          style={CustomStyles}
+          onRequestClose={closeModal}
+        >
+        <ModalWithdraw tofintechno={val}></ModalWithdraw>
         </Modal>
-        {val}
+
+        <p>{val}</p>
     </div>
   );
 };
