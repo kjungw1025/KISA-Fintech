2db41ecd4fe30b8081086c69599edb21c649c3f1
kjungw1025 <kjungw1025@naver.com>
2023-08-03 14:39:57 +0900

    fix: qr code bad recognition, position adjustment

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -3,6 +3,13 @@ import AppHeader from '../components/common/AppHeader';
 import { QRCodeSVG } from 'qrcode.react';
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import styled from "styled-components";
+const QRBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1rem;
+`;
 
 const QrCodePage = () => {
     const queryParams = useLocation().search;
@@ -12,7 +19,9 @@ const QrCodePage = () => {
     return (
         <div>
             <AppHeader title="QR 코드 생성"></AppHeader>
-            <QRCodeSVG value={fintechUseNum} size={256}/>
+            <QRBlock>
+                <QRCodeSVG value={fintechUseNum} size={256} />
+            </QRBlock>
             <p>{fintechUseNum}</p>
         </div>
     );
