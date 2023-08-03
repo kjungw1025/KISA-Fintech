ade880f30771dfffcdeaf8fe5e682d1d6ff6f55e
kjungw1025 <kjungw1025@naver.com>
2023-08-03 16:18:42 +0900

    feat: Update QrReaderPage.js

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
-import AppHeader from '../components/common/AppHeader';
-import QrCodeReader, { QRCode } from 'react-qrcode-reader';
+import React, { useState } from "react";
+import AppHeader from "../components/common/AppHeader";
+import QrCodeReader, { QRCode } from "react-qrcode-reader";
+import ModalWithdraw from "../components/Withdraw/ModalWithdraw";
+import Modal from "react-modal";
 
-const QrReader = () => {
-    const [val, setVal] = useState('');
-    const handleQrAction = (val) => {
-      console.log(val);
-    };
+const QrReaderPage = () => {
+  const [val, setVal] = useState("");
+  const [openModal, setOpenModal] = useState(false); //no camera : true
 
-    return (
-      <div>
-          <AppHeader title={"QR 리더"}></AppHeader>
-          <QrCodeReader delay={100} width={500} height={500} action={setVal} />
-          <p>{val}</p>
-      </div>
-    )
-}
+  const closeModal = () => {
+      setOpenModal(false);
+  };
 
-export default QrReader
+  const handleRead = (val) => {
+      setOpenModal(true);
+      setVal(val);
+  };
+  return (
+    <div>
+        <AppHeader title={"qr 리더"}></AppHeader>
+        <QrCodeReader delay={100} width={600} height={500} action={handleRead} />
+        <Modal isOpen={openModal} onRequestClose={closeModal}>
+          <ModalWithdraw tofintechno={val}></ModalWithdraw>
+        </Modal>
+        {val}
+    </div>
+  );
+};
+
+export default QrReaderPage;
