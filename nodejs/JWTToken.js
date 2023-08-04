85fef6f9e0e122db81afd7b9d27674492baabaf2
kjungw1025 <kjungw1025@naver.com>
2023-08-04 13:06:08 +0900

    feat: Create JWT token

---
+++ nodejs/JWTToken.js
@@ -0,0 +1,20 @@
+// https://www.npmjs.com/package/jsonwebtoken
+let jwt = require("jsonwebtoken");
+let privateKey = "dkfkjaewlsdklfjsajkldf";
+var tokenKey = "f@i#n%tne#ckfhlafkd0102test!@#%";
+jwt.sign(
+  {
+    userId: 1,
+    userEmail: "skymill2000",
+  },
+  tokenKey,
+  {
+    expiresIn: "10d",
+    issuer: "fintech.admin",
+    subject: "user.login.info",
+  },
+  function (err, token) {
+    console.log("로그인 성공\n"+token);
+    res.json(token);
+  }
+);
