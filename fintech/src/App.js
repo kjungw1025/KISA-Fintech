8dbd777bfdce5da5965e1b5cbb9ea818152dab1e
kjungw1025 <kjungw1025@naver.com>
2023-08-04 15:03:37 +0900

    feat: Create login page

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -6,6 +6,7 @@ import MainPage from "./pages/MainPage";
 import BalancePage from "./pages/BalancePage";
 import QrCodePage from "./pages/QrCodePage";
 import QrReaderPage from "./pages/QrReaderPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/balance" element={<BalancePage />}></Route>
         <Route path="/qrcode" element={<QrCodePage/>}></Route>
         <Route path="/qrreader" element={<QrReaderPage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ fintech/src/pages/LoginPage.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import AppHeader from "../components/common/AppHeader";
+import axios from "axios";
+
+const LoginPage = () => {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+    const handleChangeId = (e) => {
+        setId(e.target.value);
+    };
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
+    const handleClick = () => {
+        console.log(id, password);
+        let option = {
+            url: "/login",
+            method: "POST",
+            data: {
+                id: id,
+                password: password,
+            },
+        };
+        
+        axios(option).then((res) => {
+            console.log(res);
+        });
+
+    };
+    return (
+        <div>
+        <AppHeader title={"로그인"}></AppHeader>
+        <div>
+            id : <input onChange={handleChangeId}></input>
+            password :{" "}
+            <input type="password" onChange={handleChangePassword}></input>
+            <button onClick={handleClick}>검색</button>
+        </div>
+        </div>
+    );
+};
+
+export default LoginPage;
